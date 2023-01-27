fe9929d85317440d9a73e25aeba6774df922fe58
olo135597 <olo135597@stud.gibb.ch>
2023-01-27 08:27:05 +0100

    Update TimeBlocks.jsx

--- src/components/timeBlocks/TimeBlocks.jsx
+++ src/components/timeBlocks/TimeBlocks.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import './timeBlocks.css'
-import {TimeBlockActivity} from '../timeBlockActivity/TimeBlockActivity';
+
 
 const TimeBlocks = () => {
   return (
     <div className='timeblocks'>
-     <TimeBlockActivity />
+    
    
     </div>
   )
