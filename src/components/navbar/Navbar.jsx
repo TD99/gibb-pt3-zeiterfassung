be802bc7127232c96342a81ad8d69c7cc79a3fc1
Tim Dürr <tim@duerrs.ch>
2023-02-10 08:55:24 +0100

    Update Navbar.jsx

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -7,6 +7,7 @@ import {AiFillTool} from 'react-icons/ai';
 import {MdAdminPanelSettings} from 'react-icons/md'; 
 import {FaInfoCircle} from 'react-icons/fa'; 
 import {FaChalkboardTeacher} from 'react-icons/fa';
+import {FaUserCircle} from 'react-icons/fa';
 import {BrowserRouter as Router, Link, Switch, Route, Outlet} from 'react-router-dom';
 
 import Teacher from '../../containers/teacher/Teacher';
@@ -17,33 +18,30 @@ const Navbar = () => {
     <div className='navbar'>
       <div className='navbar-upper'>
         
+        <Link to='/'>
          <BsClockFill size={60} color="var(--textOrIcon-color)"/>
+        </Link>
          
-         <Link to='/teacher'>
-         <FaIdCard size={60} color="var(--textOrIcon-color)" /> 
-         
+         <Link to='/student'>
+          <FaIdCard size={60} color="var(--textOrIcon-color)" />
          </Link>
-         
-    
 
-        
-         
-        
-          
-      
+         <Link to='/teacher'>
+          <FaChalkboardTeacher size={60} color="var(--textOrIcon-color)"/>
+         </Link>
 
-         
-
-         
-         <AiFillTool size={60} color="var(--textOrIcon-color)"/>
-         <FaChalkboardTeacher size={60} color="var(--textOrIcon-color)"/>
+         <Link to='/admin'>
+          <AiFillTool size={60} color="var(--textOrIcon-color)"/>
+         </Link>
      </div>
      <div className='navbar-lower'> 
-     <Link to='/info'>    <FaInfoCircle size={60} color="var(--textOrIcon-color)"/></Link>
- 
-    
-       
-       <MdAdminPanelSettings size={60} color="var(--textOrIcon-color)"/>
+     <Link to='/account'>
+       <FaUserCircle size={60} color="var(--textOrIcon-color)"/>
+     </Link>
+     
+     <Link to='/info'>
+       <FaInfoCircle size={60} color="var(--textOrIcon-color)"/>
+     </Link>
      </div>
      </div>
      
