bfa85a9db74b93f99ee8d6706274e082d64ca16f
olo135597 <olo135597@stud.gibb.ch>
2023-06-09 09:21:16 +0200

    Admin Bug behoben

--- v0.0.2/src/pages/admin/Admin.tsx
+++ v0.0.2/src/pages/admin/Admin.tsx
@@ -1,24 +1,27 @@
-import './admin.css';
-import {Header, Blocks} from '../../components';
+import "./admin.css";
+import { Header, Blocks } from "../../components";
 
 const Admin: React.FC = () => {
-    const data = [
-        {
-            displayName: 'Oliver Schramm'
-        },
-        {
-            displayName: 'Yilmaz Günel'
-        },
-        {
-            displayName: 'Miriam Schluep'
-        }
-    ];
-    return (
-        <div className="admin">
-            <Header />
-            <Blocks data={data} />
-        </div>
-    )
-} 
+  const data = [
+    {
+      displayName: "Oliver Schramm",
+      id: 0,
+    },
+    {
+      displayName: "Yilmaz Günel",
+      id: 1,
+    },
+    {
+      displayName: "Miriam Schluep",
+      id: 2,
+    },
+  ];
+  return (
+    <div className="admin">
+      <Header />
+      <Blocks data={data} />
+    </div>
+  );
+};
 
 export default Admin;
